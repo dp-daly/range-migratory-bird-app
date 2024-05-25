d59fa256624e0245dcedf8e639d8f9dda48e7777
dp-daly <dan.daly@education.gov.uk>
2024-05-25 12:01:47 +0100

    Initial setup confirmed

---
+++ server.js
@@ -0,0 +1,47 @@
+/*-------------------------------- Dependencies --------------------------------*/
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const morgan = require('morgan');
+const session = require('express-session');
+
+/*-------------------------------- Controllers/Port --------------------------------*/
+
+const port = process.env.PORT ? process.env.PORT : '3000';
+
+/*-------------------------------- DB Connect --------------------------------*/
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+/*-------------------------------- Middleware --------------------------------*/
+
+app.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: true,
+    })
+  );
+  
+  app.use(express.urlencoded({ extended: false }));
+  app.use(methodOverride('_method'));
+  app.use(morgan('dev'));
+
+/*-------------------------------- Routes --------------------------------*/
+
+app.get("/", (req, res) => {
+    res.send("Hello")
+})
+
+/*-------------------------------- Listener --------------------------------*/
+
+app.listen(port, () => {
+    console.log(`The express app is ready on port ${port}!`);
+  });
