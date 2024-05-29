186caef2f8093b9203b6fa34b54059d38e9cd402
dp-daly <dan.daly@education.gov.uk>
2024-05-29 17:16:09 +0100

    Save progress

--- controllers/sighting.js
+++ controllers/sighting.js
@@ -9,7 +9,7 @@ const Comment = require('../models/comment.js');
 router.get("/:sightingId", async (req, res) => {
     if (req.session.user) {
         try {
-            const sighting = await Sighting.findById(req.params.sightingId);
+            const sighting = await Sighting.findById(req.params.sightingId).populate("publisher");
             const currentUser = req.session.user._id;
             const publisher = sighting.publisher.toString();
             const comments = sighting.comments;
